7ca18cd6889bdd96ec4ba959dcc2f64a1c6907a0
gahnsei <gahnsei@gmail.com>
2022-02-05 19:45:23 -0600

    Homepage mockup v2 with added carousel

--- homepage/crunchers-homepage.js
+++ homepage/crunchers-homepage.js
@@ -1,32 +1,32 @@
 const entreeWidg = (event) => {
-    const opBckgrnd = document.createElement(`div`);
-    const widget = document.createElement(`div`);
-    const foodImg = document.createElement(`div`);
+  const opBckgrnd = document.createElement(`div`);
+  const widget = document.createElement(`div`);
+  const foodImg = document.createElement(`div`);
 
-    let styling = window.getComputedStyle(event.target);
-    foodImg.style.backgroundImage = styling.backgroundImage;
-    foodImg.style.backgroundSize = `cover`;
-    foodImg.style.backgroundPosition = `center`;
+  let styling = window.getComputedStyle(event.target);
+  foodImg.style.backgroundImage = styling.backgroundImage;
+  foodImg.style.backgroundSize = `cover`;
+  foodImg.style.backgroundPosition = `center`;
 
-    foodImg.classList.add(`foodImg`)
-    widget.appendChild(foodImg)
+  foodImg.classList.add(`foodImg`);
+  widget.appendChild(foodImg);
 
-    opBckgrnd.classList.add(`opBckgrnd`);
-    opBckgrnd.addEventListener(`click`, exitWidget);
-    widget.classList.add(`widget`);
+  opBckgrnd.classList.add(`opBckgrnd`);
+  opBckgrnd.addEventListener(`click`, exitWidget);
+  widget.classList.add(`widget`);
 
-    opBckgrnd.appendChild(widget);
-    document.querySelector(`main`).appendChild(opBckgrnd)
-}
+  opBckgrnd.appendChild(widget);
+  document.querySelector(`main`).appendChild(opBckgrnd);
+};
 
 const exitWidget = (event) => {
-    if (event.target.classList.contains(`opBckgrnd`)) {
-        event.target.remove()
-    }
-}
+  if (event.target.classList.contains(`opBckgrnd`)) {
+    event.target.remove();
+  }
+};
 
 entreeImgDisplay = document.querySelectorAll(`.entreeImg`);
 
 for (let i = 0; i < entreeImgDisplay.length; i++) {
-    entreeImgDisplay[i].addEventListener(`click`, entreeWidg)
+  entreeImgDisplay[i].addEventListener(`click`, entreeWidg);
 }
